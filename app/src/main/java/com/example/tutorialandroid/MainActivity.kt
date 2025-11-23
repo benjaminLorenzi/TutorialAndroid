6e5dc92b9c2a17f74daaf4dbd437b7938b61b583
Benjamin Lorenzi <blorenzi@kds.com>
2025-11-23 09:03:24 +0100

    SharedPreference + Dependency Injection | Dans le mainActivity on récupère l'url des SharePreferences et on l'injecte dans la baseUrl du NetworkPost

--- app/src/main/java/com/example/tutorialandroid/MainActivity.kt
+++ app/src/main/java/com/example/tutorialandroid/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tutorialandroid
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.ui.platform.LocalContext
 import com.example.tutorialandroid.domain.NetworkPostRepository
 import com.example.tutorialandroid.navigation.MainScaffold
 import com.example.tutorialandroid.network.NetworkPost
@@ -13,17 +15,19 @@ import com.example.tutorialandroid.viewModel.PostsViewModelFactory
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContent { ... } lance Jetpack Compose
 
-        val baseUrl = "https://jsonplaceholder.typicode.com/"
+        val sharedPref = getSharedPreferences("MesPreferences", Context.MODE_PRIVATE)
+        val savedUrl = sharedPref.getString("base_url", null)
+        var baseUrl = if (savedUrl.isNullOrEmpty()) "https://jsonplaceholder.typicode.com/" else savedUrl
+
         val networkPost = NetworkPost(baseUrl)
         val repository = NetworkPostRepository(networkPost.api)
         val factory = PostsViewModelFactory(repository)
 
         val viewModel: PostsViewModel by viewModels { factory }
 
+        // lance Jetpack Compose
         setContent {
-            // On affiche directement notre composable BottomNavigationRoot()
             MainScaffold(vm = viewModel)
         }
     }
